2a98db0bf44fea2128a36871409c7729eeb4e0d7
brandanscully <brandan.scully@gmail.com>
2022-08-25 21:35:33 -0400

    Create HW1.py
    initial commit

---
+++ HW1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 25 21:32:19 2022
+
+@author: brandanscully
+"""
+
+import os
+
+#get the current working directory
+cwd = os.getcwd()
+
+#print the current owrking directory
+print ("Current working directory: ")
+print (cwd)
